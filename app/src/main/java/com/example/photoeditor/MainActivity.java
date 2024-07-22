05daa28a5088e8c7d30761a847e7768dcc37967c
Quynh Cao Thi Thanh <caothanhquynh07012003@gmail.com>
2024-07-22 23:10:18 +0700

    fix send image from main to edit activity

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         binding.btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takePic();
+                imageChooser();
             }
         });
     }
